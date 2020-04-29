7c0be8eaf6c06b60ce1ae542b3d4b007813015ae
inusha <inusham45@gmail.com>
2020-04-29 15:40:28 +0530

    Impliments splash screen

---
+++ ctse_flutter_app/lib/Pages/SplashScreen.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:ctse_flutter_app/utils/Resources.dart';
+import 'package:ctse_flutter_app/Utils/MainNavigator.dart';
+
+//Splash screen
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    //Set timer and navigate to the home page
+    Timer(Duration(seconds: 5), () => MainNavigator.goToHome(context));
+  }
+
+  @override
+  Widget build(BuildContext context) {  // View Splash screen
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Color.fromRGBO(252, 194, 3, 1.0)),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.library_books,
+                          color: Colors.blue[900],
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        Resources.name,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(
+                      valueColor: new AlwaysStoppedAnimation<Color>(Colors.blue[900]),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text(
+                      Resources.book_text,
+                      softWrap: true,
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
