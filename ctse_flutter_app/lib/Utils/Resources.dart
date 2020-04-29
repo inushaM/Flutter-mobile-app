c66c61b97c00f4d95ff6dbca948df7084b85066e
Shaini tenuwara <shainitenuwara@gmail.com>
2020-04-29 15:20:32 +0530

    create book insert,update,delete and view functions

---
+++ ctse_flutter_app/lib/Utils/Resources.dart
@@ -0,0 +1,13 @@
+//This is the Resources class..All the static variables are handle by this class
+class Resources {
+  static const String name = "මුතුහර";
+  static const String title = "මුතුහර Book Reading";
+  static const String book_text = "Online Books Reading\n For Everyone";
+  static const String menu_name = "Menu";
+  static const String menu_list_name01 = "Add Books";
+  static const String menu_list_name02 = "Add Category";
+  static const String menu_list_name03 = "About";
+  static const String about = "මුතුහර - Book reader allows to reading books online for free. this app was developed by inusha maduranga and shashini thenuwara.The app can find any kind of books from the online which already exist. This is the best way to find and read books.";
+  static const String contact_us = "If you have any question please contact us :- inusham45@gmail.com";
+
+}
